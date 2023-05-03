a92ac7e1413aa6e3ad40ae79cb6724ec8529bb39
Lyndon Fan <lyndon0808@gmail.com>
2023-05-03 23:38:05 +0100

    clean up current extract code

--- extract/extractor.py
+++ extract/extractor.py
@@ -3,7 +3,7 @@ from typing import Dict, Any
 import pandas as pd
 import logging
 
-from .paginator import get_paginate_response
+from .paginator import Paginator
 
 
 @dataclass
@@ -14,18 +14,21 @@ class Extractor:
     params: Dict[str, Any] = field(default_factory=dict)
     test: bool = False
 
+    def __post_init__(self):
+        self.paginator = Paginator(
+            url=self.url,
+            page_size=self.page_size,
+            headers=self.headers,
+            params=self.params
+        )
+
     def execute(self) -> pd.DataFrame:
         logging.info(
             f"Start fetching data from {self.url} with {self.page_size=} and paras"
         )
         logging.info(self.params)
         objects = []
-        for res in get_paginate_response(
-            self.url,
-            page_size=self.page_size,
-            headers=self.headers,
-            params=self.params,
-        ):
+        for res in self.paginator.execute():
             objects.extend(res)
             if self.test:
                 break
--- extract/paginator.py
+++ extract/paginator.py
@@ -1,92 +1,85 @@
 import requests
+from dataclasses import dataclass, field
 from math import ceil, log10
 from typing import Any, Dict, List, Generator
 import time
 
 import logging
 
+WAIT_RESPONSE_CODES: List[int] = [429, 503, 504]
 
-def get_or_retry(
-    session: requests.Session,
-    url: str,
-    start_wait_time: float = 0.5,
-    max_num_retries: int = 7,
-    headers: Dict[str, Any] = {},
-    params: Dict[str, Any] = {},
-    wait_response_codes: List[int] = [429, 503, 504],
-) -> requests.Response:
-    wait_time = start_wait_time
-    for i in range(max_num_retries):
-        response = session.get(url, headers=headers, params=params)
-        if response.status_code == 200:
-            return response
-        logging.debug(response.content.decode("utf-8").strip())
-        if response.status_code not in wait_response_codes:
-            return response
-        if i == max_num_retries - 1:
-            break
-        time.sleep(wait_time)
-        wait_time *= 2
-    return response
+@dataclass
+class Paginator:
+    url: str
+    page_size: int = 500
+    min_time_between_calls: float = 0.5
+    headers: Dict[str, Any] = field(default_factory=dict)
+    params: Dict[str, Any] = field(default_factory=dict)
 
+    def __post_init__(self):
+        if "page" in self.params:
+            self.params.pop("page")
+        if "pageSize" not in self.params:
+            self.params["pageSize"] = self.page_size
+    
+    
+    def get_or_retry(
+        self,
+        session: requests.Session,
+        params: Dict[str, Any],
+        max_num_retries: int = 7,
+        backoff_factor: float = 2,
+    ) -> requests.Response:
+        assert max_num_retries > 0, f"{max_num_retries=} must be greater than 0"
+        assert backoff_factor > 1, f"{backoff_factor=} must be greater than 1"
+        wait_time = self.min_time_between_calls
+        for i in range(max_num_retries):
+            response = session.get(self.url, headers=self.headers, params=params)
+            if response.status_code == 200:
+                return response
+            logging.debug(response.content.decode("utf-8").strip())
+            if response.status_code not in WAIT_RESPONSE_CODES:
+                return response
+            if i == max_num_retries - 1:
+                break
+            time.sleep(wait_time)
+            wait_time *= backoff_factor
+        return response
 
-def get_paginate_response(
-    url: str,
-    page_size: int = 500,
-    min_time_between_calls: float = 0.5,
-    headers: Dict[str, Any] = {},
-    params: Dict[str, Any] = {},
-) -> Generator[List[Dict[str, Any]], None, None]:
-    params.pop("page", None)
-    if "pageSize" not in params:
-        params["pageSize"] = page_size
-    with requests.Session() as session:
-        response = get_or_retry(
-            session,
-            url,
-            start_wait_time=min_time_between_calls,
-            headers=headers,
-            params=params,
-        )
-        if response.status_code != 200:
-            params.pop("pageSize")
-            response = get_or_retry(
-                session,
-                url,
-                start_wait_time=min_time_between_calls,
-                headers=headers,
-                params=params,
-            )
-            assert response.status_code == 200, "\n" + response.content.decode()
-        jsn = response.json()
-        # expects a schema of
-        # {"page": int, "total": int, "objects": [...]}
-        total = jsn["total"]
-        objs = jsn["objects"]
-        logging.info(f"{total} objects found")
-        total_n = ceil(total / len(objs))
-        num = ceil(log10(total_n + 1))
-        logging.info(f"Yielding object {' '*(num-1)}1/{total_n}")
-        yield objs
-        last_called = time.perf_counter()
-        for i in range(1, total_n):
-            params["page"] = i
-            now = time.perf_counter()
-            diff = min_time_between_calls - (now - last_called)
-            if diff > 0:
-                time.sleep(diff)
-            response = get_or_retry(
-                session,
-                url,
-                start_wait_time=min_time_between_calls,
-                headers=headers,
-                params=params,
-            )
-            assert response.status_code == 200, response.content.decode()
+    def print_debug_progress(self, i: int, total: int) -> None:
+        s = str(i)
+        num = ceil(log10(total + 1))
+        s = " " * (num - len(s)) + s
+        logging.debug(f"Yielding object {s}/{total}")
+
+    def execute(self) -> Generator[Dict[str, Any], None, None]:
+        params = {**self.params}
+        with requests.Session() as session:
+            response = self.get_or_retry(session, params)
+            if response.status_code != 200:
+                params.pop("pageSize")
+                response = self.get_or_retry(session, params)
+                assert response.status_code == 200, "\n" + response.content.decode()
             jsn = response.json()
+            # expects a schema of
+            # {"page": int, "total": int, "objects": [...]}
+            total = jsn["total"]
             objs = jsn["objects"]
-            s = str(i + 1)
-            s = " " * (num - len(s)) + s
-            logging.info(f"Yielding object {s}/{total_n}")
+            logging.debug(f"{total} objects found")
+            total_pages = ceil(total / len(objs))
+            self.print_debug_progress(1, total_pages)
             yield objs
             last_called = time.perf_counter()
+            for i in range(1, total_pages):
+                params["page"] = i
+                now = time.perf_counter()
+                diff = self.min_time_between_calls - (now - last_called)
+                if diff > 0:
+                    time.sleep(diff)
+                response = self.get_or_retry(session, params)
+                assert response.status_code == 200, response.content.decode()
+                jsn = response.json()
+                objs = jsn["objects"]
+                self.print_debug_progress(i+1, total_pages)
+                yield objs
+                last_called = time.perf_counter()
