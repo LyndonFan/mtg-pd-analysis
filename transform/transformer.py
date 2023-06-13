eaa734b7ac719a11e62579ae6c2f93b67fcc0adf
Lyndon Fan <lyndon0808@gmail.com>
2023-06-13 22:26:35 +0100

    put schema back in

--- transform/transformer.py
+++ transform/transformer.py
@@ -4,59 +4,14 @@ import logging
 
 
 class Transformer:
-    TRANSFORM_NAMES: Dict[str, Callable] = {}
-
-    @classmethod
-    def register(cls, name: str):
-        def wrapper(func: Callable[..., pd.Series]) -> Callable[..., pd.Series]:
-            if name in cls.TRANSFORM_NAMES:
-                raise KeyError(f"{name} is already in {cls.TRANSFORM_NAMES}")
-            cls.TRANSFORM_NAMES[name] = func
-            return func
-
-        return wrapper
-
     def __init__(
         self,
-        transformations: List[Dict[str, Any]],
         schema: Dict[str, Dict[str, str]],
     ):
         self.schema = schema
-        self.transformations = []
-        MUST_KEYS = ["name", "source", "target"]
-        OPT_KEYS = ["args", "kwargs"]
         sources = set(self.schema.keys())
-        for dct in transformations:
-            if not any(c in dct for c in MUST_KEYS):
-                continue
-            if dct["name"] in ["execute", "__init__"]:
-                continue
-            dct["args"] = dct.get("args", [])
-            dct["kwargs"] = dct.get("kwargs", {})
-            self.transformations.append(dct)
-            if isinstance(dct["source"], str):
-                sources.add(dct["source"])
-            else:
-                sources.update(*dct["source"])
         self.source_columns = list(sources)
-
-    def _transform_repr(
-        self, function_name: str, target_col: str, source_col: str, args, **kwargs
-    ) -> str:
-        res_string = f'df["{target_col}"] = {function_name}(df["{source_col}"]'
-
-        def sstr(x) -> str:
-            return f'"{x}"' if isinstance(x, str) else str(x)
-
-        if args:
-            for a in args:
-                res_string += ", " + sstr(a)
-        if kwargs:
-            for k, v in kwargs.items():
-                res_string += f", {k}={sstr(v)}"
-        res_string += ")"
-        return res_string
-
+    
     def execute(self, df: pd.DataFrame) -> pd.DataFrame:
         df["url"] = "https://pennydreadfulmagic.com" + df["url"]
         df["omw"] = df["omw"].str.replace("%", "")
@@ -71,5 +26,20 @@ class Transformer:
         df["createdDate"] = df["createdDatetime"].dt.strftime("%Y%m%d")
         df["updatedDatetime"] = pd.to_datetime(df["updatedDate"], unit="s")
         df["updatedDate"] = df["updatedDatetime"].dt.strftime("%Y%m%d")
-
+        
+        keep_columns = []
+        for source, dct in self.schema.items():
+            if dct["dtype"] == "object":
+                logging.info(f"Setting {source} as dtype=object")
+                keep_columns.append(source)
+                continue
+            else:
+                logging.info(f"Setting {source} as dtype={dct['dtype']}")
+                df.loc[df[source].isna(), source] = None
+                df[source] = df[source].astype(dct["dtype"])
+            if dct["dtype"] == "category" and "categories" in dct:
+                logging.info(f"Setting categories for {source} as {dct['categories']}")
+                df[source] = df[source].cat.set_categories(dct["categories"])
+            keep_columns.append(source)
+        df = df[keep_columns].copy()
         return df
