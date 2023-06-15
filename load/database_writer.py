09c9fc64ccc3a8b3b667a369c8d4ef2aa463b7d7
Lyndon Fan <lyndon0808@gmail.com>
2023-06-16 00:10:41 +0100

    insert conflict can update/do nothing

--- load/database_writer.py
+++ load/database_writer.py
@@ -7,21 +7,34 @@ from .writer import Writer
 
 
 class DatabaseWriter(Writer):
-    def __init__(self, table_name):
+    def __init__(self, table_name: str, id_column: str = "id") -> None:
         self.table_name = table_name
+        self.id_column = id_column
         self.database = Database()
 
     def connection(self):
         return self.database.connection()
 
-    def execute(self, df: pd.DataFrame, inside_transaction: bool = False) -> bool:
-        columns = list(df.columns)
+    def execute(
+        self,
+        df: pd.DataFrame,
+        inside_transaction: bool = False,
+        on_conflict_update: bool = False,
+    ) -> bool:
+        columns = df.columns.tolist()
+        assert self.id_column in columns, f"{self.id_column=} not in {columns=}"
         values = [tuple(row) for row in df.values]
         placeholders = ",".join(["%s"] * len(columns))
+        rest_columns = [c for c in columns if c != self.id_column]
         insert_sql = f"""
             INSERT INTO {self.table_name} ({','.join(columns)}) VALUES ({placeholders})
-            ON CONFLICT DO UPDATE
-        """
+            ON CONFLICT ({self.id_column}) """
+        if on_conflict_update:
+            insert_sql += f"""DO UPDATE
+                SET {', '.join(f'{c}=EXCLUDED.{c}' for c in rest_columns)}
+            """
+        else:
+            insert_sql += f"""DO NOTHING"""
         logging.info(insert_sql)
         conn = self.database.connection()
         try:
--- load/load.py
+++ load/load.py
@@ -11,17 +11,17 @@ class Loader:
         people_df = df[["personId", "person"]].drop_duplicates()
         people_df.columns = ["id", "name"]
         DatabaseWriter("people").execute(people_df)
-        archetype_df = df[["archetypeId", "archetype"]].drop_duplicates()
+        archetype_df = df[["archetypeId", "archetypeName"]].drop_duplicates()
         archetype_df.columns = ["id", "archetype"]
-        DatabaseWriter("archetypes").execute(archetype_df)
+        DatabaseWriter("archetypes").execute(archetype_df, on_conflict_update=True)
         common_connection = Database.common_connection()
-        info_df = df.drop(columns=["maindeck", "sideboard", "person", "archetype"])
+        info_df = df.drop(columns=["maindeck", "sideboard", "person", "archetypeName"])
         with common_connection:
-            DatabaseWriter("decks").execute(info_df, True)
+            DatabaseWriter("decks").execute(info_df, True, True)
             for board in ["maindeck", "sideboard"]:
                 cards_df = df[["id", board]].explode(board)
                 board_df = pd.DataFrame(cards_df[board].values.tolist())
                 cards_df = pd.concat([cards_df, board_df], axis=1)
                 cards_df = cards_df.drop(columns=board)
                 cards_df.columns = ["deckId", "n", "name"]
-                DatabaseWriter(board + "s").execute(cards_df, True)
+                DatabaseWriter(board + "s", "deckId").execute(cards_df, True, True)
