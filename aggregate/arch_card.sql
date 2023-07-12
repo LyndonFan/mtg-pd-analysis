bba395e9352add9f8721f8b1325727376ff271ab
Lyndon Fan <lyndon0808@gmail.com>
2023-07-12 23:33:03 +0100

    update sql

--- aggregate/arch_card.sql
+++ aggregate/arch_card.sql
@@ -15,10 +15,24 @@ grouped as (
     from joined
     group by "cardName", "archetypeId"
 )
-select *,
+select
+"cardName",
+"archetype",
+"numDecks",
+"numDecks"::float / other."numArchetypeDecks" as "avgIncludeRate",
+n::float / "numDecks" as "avgIncludeN",
 CASE
     WHEN matches = 0 THEN 0
     ELSE wins::float / matches
 END AS "winRate",
 (wins::float+1.0)/(matches+2.0) as "smoothWinRate"
-from grouped
+from grouped join (
+    select b.*, archetype from
+    (
+        select "archetypeId", count(1) as "numArchetypeDecks"
+        from decks group by "archetypeId"
+    ) b
+    join archetypes
+    on b."archetypeId" = archetypes.id
+) other
+on grouped."archetypeId" = other."archetypeId"
