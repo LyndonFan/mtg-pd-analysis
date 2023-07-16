b443433db158634fe16820d57bfb78f8174a5cd3
Lyndon Fan <lyndon0808@gmail.com>
2023-07-16 23:58:28 +0100

    placeholder terraform

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.4.0"
+    }
+    google = {
+      source = "hashicorp/google"
+      version = "4.73.1"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_name
+  region  = var.region
+}
+
+provider "archive" {}
+
+resource "google_storage_bucket" "source_bucket" {
+  name     = "source-bucket"
+  location = var.region
+}
+
+data "archive_file" "scraper_zip" {
+  type          = "zip"
+  source_dir    = "${path.module}/${var.scraper_folder}"
+  output_path   = "${path.module}/scraper.zip"
+}
+
+resource "google_storage_bucket_object" "scraper_zip" {
+  name   = "scraper.zip"
+  bucket = google_storage_bucket.source_bucket.name
+  source = scraper_zip.output_path
+}
+
+resource "google_cloudfunctions_function" "my_function" {
+  name        = "my-function"
+  description = "My Cloud Function"
+  runtime     = "python39"
+  available_memory_mb = 1024
+  source_archive_bucket = google_storage_bucket.source_bucket.name
+  source_archive_object = google_storage_bucket_object.scraper_zip.name
+  entry_point = "main"
+  service_account_email = "foo-bar-sa@your-project-id.iam.gserviceaccount.com"
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+    type = string
+    default = "mtg-pd-analysis"
+}
+
+variable "region" {
+    type = string
+    default = "us-east1"
+}
+
+variable "scraper_folder" {
+    type = string
+    default = "scraper"
+}
